776426cb2bfb4092ed967f4cab0a16e320104e68
Safwan Akram <safwanakram05@gmail.com>
2025-09-02 23:44:21 +0530

    Day 1 AIML

---
+++ MiniChallenge#2.py
@@ -0,0 +1,4 @@
+#A program to from the user and displaying the output...
+
+name = input("What's your name?\n")
+print("Your name is", name)
---
+++ MiniChallenge#5.py
@@ -0,0 +1,7 @@
+#Swap two variables without using third variable..
+
+x = 5
+y = 8
+x, y = y, x
+print('x =', x)
+print('y = ', y)
